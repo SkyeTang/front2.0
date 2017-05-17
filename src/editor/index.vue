5b5c9c29d68a6f37c79146e3ee50454758099f22
skye <forbetter@yeah.net>
2017-05-18 00:41:34 +0800

    添加编辑器插件 上传图片

--- src/editor/index.vue
+++ src/editor/index.vue
@@ -1,23 +1,60 @@
 <template>
     <div class="edit-container">
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <textarea :id="id"></textarea>
     </div>
 </template>
 
 <script>
     export default{
+        props: {
+            id: {
+                type: String,
+                default: 'richEditor'
+            },
+            value: {
+                type: String,
+                default: ''
+            },
+            toolbar: {
+                type: Array,
+                required: false,
+                default() {
+                    return ["undo redo | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | fontselect fontsizeselect | forecolor backcolor | bullist numlist | outdent indent | removeformat | link unlink uploadimage | preview fullscreen  "]
+                }
+            },
+        },
+        data(){
+            return {
+                hasChange: false,
+                hasInit: false
+            }
+        },
         mounted(){
             tinymce.init({
-                selector: 'textarea',
+                language: 'zh_CN',
+                selector: `#${this.id}`,
                 height: 500,
-                menubar: false,
+                menubar: '',
+                upload_image_url: '/upload/cloud',
                 plugins: [
-                    'advlist autolink lists link image charmap print preview anchor',
-                    'searchreplace visualblocks code fullscreen',
-                    'insertdatetime media table contextmenu paste code'
+                    'advlist autolink autosave lists link image charmap print preview anchor',
+                    'searchreplace visualblocks code fullscreen textcolor colorpicker textpattern code uploadimage',
+                    'contextmenu paste'
                 ],
-                toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-                content_css: '//www.tinymce.com/css/codepen.min.css'
+                toolbar: this.toolbar,
+                default_link_target: '_blank',
+                content_css: '//www.tinymce.com/css/codepen.min.css',
+                init_instance_callback: editor => {
+                    if (this.value) {
+                        editor.setContent(this.value)
+                    }
+                    this.hasInit = true;
+                    editor.on('Change', () => {
+                        this.hasChange = true;
+                        console.log(editor.getContent({format: 'raw'}))
+                        this.$emit('input', editor.getContent({format: 'raw'}));
+                    });
+                },
             });
         }
     }
