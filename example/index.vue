79967bb4e21ad351ace552f4b4b02a6f11c7d7e1
skye <forbetter@yeah.net>
2017-07-13 22:56:07 +0800

    optimize router

--- example/index.vue
+++ example/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="header">
-            S-UI
+            SKUI
         </div>
         <div class="main">
             <div class="sidebar">
@@ -27,11 +27,13 @@
     @import './assets/css/reset.css';
     .header{
         position: fixed;
+        display: flex;
+        align-items: center;
         top:0;
         height: 60px;
         width: 100%;
         padding: 10px 60px;
-        border-bottom: 1px solid gray;
+        box-shadow: 0 0 1px rgba(0,0,0,.25);
     }
     .main{
         margin-top: 61px;
@@ -42,6 +44,7 @@
         top:61px;
         left: 0;
         padding:10px 20px 30px 60px;
+
     }
     .content{
         margin-left: 260px;
